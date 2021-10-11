2304b65296a4b796677212ea172d2358641b3738
emre candan <wastorist@gmail.com>
2021-10-11 18:31:01 +0300

    view

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+//MIDDLE WARE
 app.use(express.static('public'));
 
-
+//ROUT
 app.get('/', (req, res) => {
-  res.send(path.resolve(__dirname,'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 const port = 3000;
 app.listen(port, () => {
